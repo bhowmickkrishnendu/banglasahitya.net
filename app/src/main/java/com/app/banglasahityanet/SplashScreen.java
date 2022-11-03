e54f23aeff71ffc4befc23f6c455029e4bec1040
bhowmickkrishnendu <krishnendu2u@gmail.com>
2022-11-04 00:39:19 +0530

    Splash Screen added 1.3

---
+++ app/src/main/java/com/app/banglasahityanet/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.app.banglasahityanet;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class SplashScreen extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int SPLASH_TIME_OUT = 5000;
+        /*
+         * Showing splash screen with a timer. This will be useful when you
+         * want to show case your app logo / company
+         */
+        new Handler().postDelayed(() -> {
+            // This method will be executed once the timer is over
+            // Start your app main activity
+            Intent i = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(i);
+
+            // close this activity
+            finish();
+        }, SPLASH_TIME_OUT);
+    }
+}
