f3794716be9de15bb6a4e15de90229dcdfe7d245
bhowmickkrishnendu <krishnendu2u@gmail.com>
2022-11-03 09:38:40 +0530

    Initial Release V1.0

---
+++ app/src/main/java/com/app/banglasahityanet/MainActivity.java
@@ -0,0 +1,63 @@
+package com.app.banglasahityanet;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebview=(WebView) findViewById(R.id.webview);
+        mywebview.setWebViewClient(new WebViewClient());
+        mywebview.loadUrl("https://banglasahitya.net/");
+        WebSettings webSettings= mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+    public class mywebclient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon){
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    @Override
+    public void onBackPressed(){{
+        if (mywebview.canGoBack()) {
+            mywebview.goBack();
+
+        }
+        else
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure you want to exit?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+
+    }
+}}
